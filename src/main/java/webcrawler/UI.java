11ab0a4d707470aa37c63708dcd13c98e065c970
Alex <nyaralex@gmail.com>
2014-09-21 22:44:33 +0300

    finall

--- src/main/java/webcrawler/UI.java
+++ src/main/java/webcrawler/UI.java
@@ -1,61 +1,65 @@
 package webcrawler;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.List;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JProgressBar;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.BorderFactory;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class UI extends JFrame {
-
 	static private int BOR = 10;
 	private WebCrawler wCrawler = new WebCrawler();
 
 	public void createGUI() {
-
 		final JFrame frame = new JFrame("QViewer");
 		frame.setMinimumSize(new Dimension(800, 800));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JPanel panel = new JPanel();
 		panel.setBorder(BorderFactory.createEmptyBorder(BOR, BOR, BOR, BOR));
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-		panel.add(Box.createVerticalGlue());
+		panel.setLayout(new BorderLayout());
 
 		final JTextField queryTextField = new JTextField();
-		panel.add(queryTextField);
+		panel.add(queryTextField, BorderLayout.PAGE_START);
 
-		JButton searchButton = new JButton("Search");
-		panel.add(searchButton);
-
-		panel.add(Box.createVerticalGlue());
+		final JButton searchButton = new JButton("Search");
+		panel.add(searchButton, BorderLayout.LINE_END);
 
 		final JTextArea resultArea = new JTextArea();
-		panel.add(resultArea);
+		panel.add(resultArea, BorderLayout.CENTER);
 
 		searchButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String result;
-				try {
-					result = wCrawler.getURLs(queryTextField.getText());
-					resultArea.setText(result);
-				} catch (IOException exc) {
-					resultArea.setText("Error. Impossible get the result.");
-					exc.printStackTrace();
-				}
+				Thread t = new Thread() {
+					public void run() {
+						searchButton.setEnabled(false);
+						String result;
+						try {
+							String query = queryTextField.getText();
+							if (query.isEmpty()) {
+								resultArea.setText("Empty query!");
+								return;
+							}
+							result = wCrawler.getURLs(query);
+							resultArea.setText(result);
+						} catch (IOException exc) {
+							resultArea
+									.setText("Error! Impossible get the result.");
+							exc.printStackTrace();
+						} finally {
+							searchButton.setEnabled(true);
+						}
+					}
+				};
+				t.start();
 			}
 		});
 
@@ -66,6 +70,7 @@ public class UI extends JFrame {
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+
 	}
 
 	public static void main(String[] args) {
--- src/main/java/webcrawler/WebCrawler.java
+++ src/main/java/webcrawler/WebCrawler.java
@@ -18,29 +18,28 @@ import org.jsoup.select.Elements;
 
 public class WebCrawler {
 
-	public String getURLs(String query)
-			throws UnsupportedEncodingException, IOException {
+	public String getURLs(String query) throws UnsupportedEncodingException,
+			IOException {
 		List<String> links = parsingPage(readPage(query, 0));
+		// Для некоторых запросов возвращало менее 10 ссылок.
 		links.addAll(parsingPage(readPage(query, 10)));
-		if(links.size() <=10)
+		if (links.size() <= 10)
 			return conversion(links);
-		else 
+		else
 			return conversion(links.subList(0, 10));
 	}
 
 	private String readPage(String stringQuery, int startLink)
 			throws IOException {
+		// Воспользовался методом получения страницы предложенным в тексте задания.
 		String google = "https://www.google.by/search?q=";
 		String charset = "UTF-8";
-		String startParam = "&start=" + startLink;
-
+		String startLinkParam = "&start=" + startLink;
 		String query = URLEncoder.encode(stringQuery, charset);
-		URL url = new URL(google + query + startParam);
-
+		URL url = new URL(google + query + startLinkParam);
 		URLConnection agent = url.openConnection();
 		agent.addRequestProperty("User-Agent",
 				"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
-
 		InputStreamReader stream = new InputStreamReader(
 				agent.getInputStream(), charset);
 		int c;
@@ -48,7 +47,6 @@ public class WebCrawler {
 		while ((c = stream.read()) != -1) {
 			page.append((char) c);
 		}
-
 		return page.toString();
 	}
 
@@ -56,18 +54,14 @@ public class WebCrawler {
 			throws UnsupportedEncodingException {
 		List<String> links = new ArrayList<String>();
 		Elements elements = Jsoup.parse(htmlPage).select("li.g>h3>a");
-
 		for (Element element : elements) {
 			String url = element.attr("href");
-
 			url = java.net.URLDecoder.decode(url, "UTF-8");
 			if (!url.startsWith("/url?q="))
 				continue;
 			url = url.substring(url.indexOf("/url?") + 7, url.indexOf("&sa="));
-
 			links.add(url);
 		}
-
 		return links;
 	}
 
@@ -79,10 +73,9 @@ public class WebCrawler {
 		return result.toString();
 	}
 
-	public static void main(String[] args) throws IOException {
-		WebCrawler wc = new WebCrawler();
-		System.out.println(wc.getURLs("What is mars?"));
-
-	}
+	// public static void main(String[] args) throws IOException {
+	// WebCrawler wc = new WebCrawler();
+	// System.out.println(wc.getURLs("What is mars?"));
+	// }
 
 }
